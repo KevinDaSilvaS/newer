1b26bea14c110196c7017f494858e10c2176043e
Your Name <kevin.kds80@gmail.com>
2021-02-18 20:38:52 -0300

    Add an extra 'D' in additional word

--- src/FormatDocs.hs
+++ src/FormatDocs.hs
@@ -10,7 +10,7 @@ docs = do
     putStrLn $ (bold . blue) "[--v] Displays the current version of newer."
     putStrLn $ (bold . blue) "[--help] Displays this info on screen."
     putStrLn $ (bold . blue) "[latest] Gets the latest news in currents api."
-    putStrLn $ (bold . blue) "[search] Gets news based on aditional parameters.[lang, keyword, country]"
+    putStrLn $ (bold . blue) "[search] Gets news based on additional parameters.[lang, keyword, country]"
     putStrLn $ (bold . blue) "[lang] Filters news by language. Ex: lang=pt"
     putStrLn $ (bold . blue) "[keyword] Filters news by keyword. Ex: keyword=brazil"
     putStrLn $ (bold . blue) "[country] Filters news by country. Ex: country=br"
